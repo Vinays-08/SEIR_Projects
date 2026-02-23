4bc826a7d3ffc18bd72ba13da494273046a19159
Vinay Prajapati <vinayprajapati0806@gmail.com>
2026-02-23 22:58:17 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,35 +2,28 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-#take URL from the user
 if len(sys.argv) < 2:
     print("Usage: python scraper.py URL")
     sys.exit()
 url_to_fix = sys.argv[1]
 
-#Get the page data
 my_headers = {"User-Agent": "Mozilla/5.0"}
 page = requests.get(url_to_fix, headers=my_headers)
 
-#Start the parser
-data = BeautifulSoup(page.text, "html.parser")
-
-  # Get Title 
+data = BeautifulSoup(page.text, "html.parser") 
 if data.title:
     print("Page Title:", data.title.string)
 
-#Clean and Get Body Text
 for extra in data(["script", "style"]):
     extra.decompose()
 
-#Get all text from the body
 full_body = data.get_text(separator=" ", strip=True)
 print("\nBody Content:")
 print(full_body)
 
-#Get All Links
 print("\nList of Links:")
-for link in data.find_all('a'):  #check if the'a'tag actually has a link (href)
+for link in data.find_all('a'):  
     address = link.get('href')
     if address:
+
         print(address)
